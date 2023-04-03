9b92845c8ba1f4bfbeb3f2612b53915a74696f13
Raphael Alampay <raphael.alampay@gmail.com>
2023-04-03 10:52:10 +0800

    fixed index.html

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-console.log("Hello world!")
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './js/App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
+    <App />
+)
