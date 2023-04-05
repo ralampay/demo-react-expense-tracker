d68559c5c9bb7dda00f56ad892216108ad8658de
Raphael Alampay <raphael.alampay@gmail.com>
2023-04-05 11:11:02 +0800

    transfer delete function to show page for expenses

--- src/js/ExpenseList.js
+++ src/js/ExpenseList.js
@@ -1,12 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { Modal } from 'react-bootstrap';
 import { getAll } from './services/ExpensesService';
 
 const ExpenseList = (props) => {
-
-    const [isModalOpen, setIsModalOpen] = useState(false);
-    const [currentId, setCurrentId] = useState(-1);
     const [items, setItems] = useState([]);
 
     const navigate = useNavigate();
@@ -22,39 +18,6 @@ const ExpenseList = (props) => {
 
     return (
         <React.Fragment>
-            <Modal
-                show={isModalOpen}
-            >
-                <Modal.Header>
-                    <Modal.Title>
-                        Confirmation
-                    </Modal.Title>
-                </Modal.Header>
-                <Modal.Body>
-                    <p>
-                        Are you sure?
-                    </p>
-                </Modal.Body>
-                <Modal.Footer>
-                    <button
-                        className="btn btn-success"
-                        onClick={() => {
-                            props.deleteItem(currentId);
-                            setIsModalOpen(false);
-                        }}
-                    >
-                        Yes
-                    </button>
-                    <button
-                        className="btn btn-danger"
-                        onClick={() => {
-                            setIsModalOpen(false);
-                        }}
-                    >
-                        No
-                    </button>
-                </Modal.Footer>
-            </Modal>
             <table className="table table-sm table-bordered mt-2">
                 <thead>
                     <tr>
--- src/js/ExpenseShow.js
+++ src/js/ExpenseShow.js
@@ -1,20 +1,83 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
+import { fetchOne, remove as deleteItem } from './services/ExpensesService';
+import { Modal } from 'react-bootstrap';
 
 const Show = () => {
 
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const [expense, setExpense] = useState({
+        id: "",
+        item: "",
+        amount: "",
+        category_id: ""
+    })
+
     const navigate = useNavigate();
 
-    let { id } = useParams(); 
+    let { id } = useParams();
+
+    useEffect(() => {
+        fetchOne(id).then((response) => {
+            setExpense(response.data);
+        }).catch((response) => {
+            alert("error")
+            console.log(response)
+        })
+    }, [])
 
     return (
         <React.Fragment>
+            <Modal
+                show={isModalOpen}
+            >
+                <Modal.Header>
+                    <Modal.Title>
+                        Confirmation
+                    </Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <p>
+                        Are you sure?
+                    </p>
+                </Modal.Body>
+                <Modal.Footer>
+                    <button
+                        className="btn btn-success"
+                        onClick={() => {
+                            deleteItem(id);
+                            navigate('/');
+                        }}
+                    >
+                        Yes
+                    </button>
+                    <button
+                        className="btn btn-danger"
+                        onClick={() => {
+                            setIsModalOpen(false);
+                        }}
+                    >
+                        No
+                    </button>
+                </Modal.Footer>
+            </Modal>
             <h1>
-                Show Page {id}
+                {expense.item}
             </h1>
+            <h2 className="text-muted">
+                {expense.amount}
+            </h2>
             <hr/>
+            <button
+                className="btn btn-danger w-100"
+                onClick={() => {
+                    setIsModalOpen(true);
+                }}
+            >
+                Delete
+            </button>
             <button 
-                className="btn btn-info w-100"
+                className="btn btn-info w-100 mt-4"
                 onClick={() => {
                     navigate('/');
                 }}
